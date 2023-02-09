df6fedcf7e18b1476bc5950426baf87cfd5faa99
Jay C33 <85657500+jayc3-3@users.noreply.github.com>
2023-02-09 13:59:25 -0800

    Update ClientNetworking.py

--- ClientNetworking.py
+++ ClientNetworking.py
@@ -1,36 +1,59 @@
-#Import modules
+#Import socket module
 import socket
 
-#Define "Networking" class
-class Networking:
+#Create class for networking
+class Networking():
     #Define class variables
     DataToSend = ""
     RecievedData = ""
     RecievedDataStr = ""
     
+    #Create a inner function that is ran on startup
     def __init__(self):
-        #Setup client
+        #Create a client socket
         self.Client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.ServerAddress = "127.0.0.1"
-        self.ServerPort = 10957
-        self.Server = (self.ServerAddress, self.ServerPort)
         
+        #Set the time out for recieving data from server
+        self.Client.settimeout(10)
+        
+        #Set the server address and port
+        self.ServerAddress = input("Input the address of the server: ")
+        self.ServerPort = int(input("Input the port of the server: "))
+        
+        #Define shorter name for server
+        self.Server = (self.ServerAddress, self.ServerPort)
+
+    #Create a inner function to connect to the server
     def Connect(self):
-        #Attempt connection to server
+        #Attempt a connection to the server when .Connect() is called
         try:
+            #Attempt to connect to the server
             self.Client.connect(self.Server)
         except:
-            pass
+            #Print if connection fails
+            print("Ecountered an error while connecting to server")
+            quit()
     
-    def Send(self, data):
-        #Attempt to send data to server
+    #Create a inner function for sending data to server
+    def Send(self, Data):
+        #Attempt to send data when .Send() is called
         try:
-            self.data = data
-            self.Client.sendto(self.data.encode(), self.Server)
+            #Attempt to send data
+            self.Data = Data
+            self.Client.sendto(self.Data.encode(), self.Server)
         except:
-            pass
-    
+            #Print message if sending data fails
+            print("Ecountered an error while sending data")
+            quit()
+
+    #Create a inner function to wait for data from server
     def WaitForData(self):
-        #Wait for data to be sent from server
-        RecievedData, Server = self.Client.recvfrom(1024)
-        Networking.RecievedDataStr = str(RecievedData, 'utf-8')
+        #Wait for data from server when .WaitForData() is called
+        try:
+            #Wait for data from server
+            RecievedData, Server = self.Client.recvfrom(1024)
+            Networking.RecievedDataStr = str(RecievedData, 'utf-8')
+        except:
+            #Print error message if the connection times out
+            print("Encountered an error while waiting for data")
+            quit()
